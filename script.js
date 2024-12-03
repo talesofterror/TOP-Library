fbf3fa27ce4f582746e692bc18d8d699a2c30a53
mike <mich.rivera84@gmail.com>
2024-12-02 23:06:20 -0500

    .

---
+++ script.js
@@ -0,0 +1,24 @@
+console.log("hi")
+
+/* 
+
+Library Array
+
+Book Object:
+	Title
+	Author
+	Description
+	Pages
+	Read Status
+
+Loop Function
+	Display Books
+	Update library display
+
+New Book Function
+
+Delete Book Function
+
+Book Info Expand Function
+
+*/
