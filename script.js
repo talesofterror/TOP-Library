a9082a5a49692fec68fd531baf715e763df23891
mike <mich.rivera84@gmail.com>
2024-12-05 21:22:08 -0500

    basic library functions. need to sketch the layout and make some hopefully cute book icons

--- script.js
+++ script.js
@@ -22,3 +22,33 @@ Delete Book Function
 Book Info Expand Function
 
 */
+
+
+let library = []
+
+function Book(name, author) {
+	this.name = name
+	this.author = author
+	this.pages = undefined
+	this.readStatus = undefined
+	this.description = undefined
+}
+
+function addBook (name, author) {
+	let newBook = new Book(name, author)
+	library.push(newBook)
+}
+
+addBook("Le Miserable", "Hugo Boss")
+addBook("There and Back Again", "Bilbo Baggins")
+addBook("The Necronomicon", "Bruce Campbell")
+
+function deleteBook (index) {
+	library.splice(index, index)
+}
+
+function refreshLibrary() {
+	for (const [index, book] of library.entries()) {
+		console.log(index + ": " + book.name + " by " + book.author)
+	}
+}
