8d0ae8e25d2ddd686314ebe841bd77524fc0c50e
mike <mich.rivera84@gmail.com>
2024-12-22 00:55:28 -0500

    .

--- script.js
+++ script.js
@@ -31,44 +31,40 @@ let library = []
 // can i create a chain function for pages, 
 // redStatus and description? 
 function Book(name, author) {
+
+	this.entryContainer = createEntryElement("book-entry"),
+	this.entryInfoContainer = createEntryElement("book-entry-info"),
+	this.entryInfoName = createEntryElement("book-entry-info-title"),
+	this.entryInfoAuthor = createEntryElement("book-entry-info-author"),
+	this.entryInfoPages = createEntryElement("book-entry-info-pages"),
+	this.entryDescription = createEntryElement("book-entry-description"),
+	this.entryReadStatus = createEntryElement("book-entry-readstatus"),
+
 	this.name = {
 		value: name,
-		element: createEntryElement("book-entry-info-title")
+		element: this.entryInfoName
 	},
 	this.author = {
 		value: author,
 		element: createEntryElement("book-entry-info-author")
 	},
 	this.pages = {
-		value: undefined,
-		element: createEntryElement("book-entry-info-pages")
+		element: createEntryElement("book-entry-info-pages"),
+		value: undefined
+	},
+	this.description = {
+		element: this.entryDescription,
+		value: undefined
 	},
 	this.readStatus = {
 		value: undefined,
-		element: createEntryElement("book-entry-info-readstatus")
+		element: createEntryElement("book-entry-readstatus")
 	},
-	this.description = {
-		value: undefined,
-		element: createEntryElement("book-entry-info-description")
-	},
-	this.setPages = (pages) => {
-		this.pages = pages
-	}
-	this.SetReadStatus = (status) => {
-		this.readStatus = status
-	}
-	this.SetDescription = (description) => {
-		this.description = description
-	},
-	this.entryContainer = createEntryElement("book-entry")
-	this.entryInfoContainer = createEntryElement("book-entry-info")
 	this.entryIcon = {
 		element: createEntryElement("book-entry-icon"),
 		img: document.createElement("img")
 	}
 }
-
-
 
 let bookArea = document.getElementById("book-area")
 
@@ -79,7 +75,12 @@ function refreshLibrary() {
 		console.log(index + ": " + book.name + " by " + book.author)
 
 		appendEntryElements(bookArea, book.entryContainer)
-		appendEntryElements(book.entryContainer, book.entryIcon.element, book.entryInfoContainer, book.description.element, book.readStatus.element)
+		appendEntryElements(
+			document.querySelectorAll(".book-entry")[index], 
+				book.entryIcon.element, 
+				book.entryInfoContainer, 
+				book.description.element, 
+				book.readStatus.element)
 
 		appendEntryElements(book.entryInfoContainer, book.name.value, book.author.value, book.pages.value)
 		appendEntryElements(book.entryIcon.element, book.entryIcon.img)
@@ -129,14 +130,17 @@ inputAddButton.addEventListener("click", (e) => {
 // more parameters?
 function addDefaultBook (name, author) {
 	let newBook = new Book(name, author)
+	newBook.pages = 100
+	newBook.description = "Default book description"
+	newBook.readStatus = true
 	library.push(newBook)
 }
 
 function inputAddBook () {
 	let newBook = new Book(inputBookAuthor.value, inputBookPages.value)
-	newBook.pages = inputBookPages.value
-	newBook.description = inputBookDescription.value
-	newBook.readStatus = inputBookReadStatus.checked // .value is not related to whether the box is checked. .checked is
+	newBook.pages.value = inputBookPages.value
+	newBook.description.value = inputBookDescription.value
+	newBook.readStatus.value = inputBookReadStatus.checked // .value is not related to whether the box is checked. .checked is
 	library.push(newBook)
 	refreshLibrary()
 	addBookDialog.close()
@@ -147,9 +151,6 @@ Number(12).toString(16)
 // converts the number 12 to its hexadecimal representation
 
 addDefaultBook("Le Miserable", "Hugo Boss")
-library[0].SetDescription("This is the description for the miserables")
-library[0].setPages(2000)
-library[0].SetReadStatus(true)
 addDefaultBook("There and Back Again", "Bilbo Baggins")
 addDefaultBook("The Necronomicon", "Bruce Campbell")
 
